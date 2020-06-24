e9fcc4d339c19a87dafab5e214818f8ca4fd854e
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-24 15:26:21 +0530

    container-fluid

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header/Header'
 import Banner from './Banner/Banner';
+import { Container } from 'reactstrap';
 
 class HomePage extends Component {
   render() {
     return (
     <div>
+      <Container fluid={true}>
         <Header />
         <Banner />
+        </Container>
     </div>
     );
   }
