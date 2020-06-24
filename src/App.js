351170aaf0e555da028c9539eeaa06c14e573cef
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-24 20:42:41 +0530

    error clear

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './HomePage/HomePage';
-import CustomCursor from './CustomCursor/CustomCursor';
 
 class App extends Component {
   render() {
     return (
     <div>
-      <CustomCursor />
       <HomePage />
     </div>
     );
