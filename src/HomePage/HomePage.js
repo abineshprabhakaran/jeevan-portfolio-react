d9b359ae616fe92a44f20f254b489b411832f536
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-25 00:21:16 +0530

    3d fix

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -21,7 +21,7 @@ class HomePage extends Component {
         y += (lFollowY - y) * friction;
 
         $wrap.css({
-            'transform': 'translate(0,0) perspective(5000px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
+            'transform': 'translate(0,0) perspective(1500px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
         });
         window.requestAnimationFrame(animate);
     }
