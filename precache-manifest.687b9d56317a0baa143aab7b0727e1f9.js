6b96ae057b998c7f6273bf340b3a31e2a00b550b
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-24 14:50:55 +0530

    Updates

---
+++ precache-manifest.687b9d56317a0baa143aab7b0727e1f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfab14f92575e3e25fd2b38d7eaed15f",
+    "url": "/jeevan-portfolio-react/index.html"
+  },
+  {
+    "revision": "ae7bbb4efc36890aaa70",
+    "url": "/jeevan-portfolio-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c53ca43ea99ab26ccfd",
+    "url": "/jeevan-portfolio-react/static/js/2.ee4b337f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jeevan-portfolio-react/static/js/2.ee4b337f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7bbb4efc36890aaa70",
+    "url": "/jeevan-portfolio-react/static/js/main.74af1dd8.chunk.js"
+  },
+  {
+    "revision": "7169e2bae01734aeaa59",
+    "url": "/jeevan-portfolio-react/static/js/runtime-main.9bcfdca2.js"
+  },
+  {
+    "revision": "85b9acd719e30f8d1ced0ef188e15ea4",
+    "url": "/jeevan-portfolio-react/static/media/jeevan.85b9acd7.png"
+  }
+]);
