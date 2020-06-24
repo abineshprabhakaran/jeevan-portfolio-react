c6501f21cddb1c1985fe53eb11a8b9936a3a6f82
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-24 16:47:16 +0530

    header

--- src/HomePage/Header/Header.js
+++ src/HomePage/Header/Header.js
@@ -1,9 +1,10 @@
-import React , { Component } from 'react';
-import { ListGroup , ListGroupItem , Button } from 'reactstrap';
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
 import $ from 'jquery';
+import jrlogo from '../../images/favicon.png';
 
 class Header extends Component {
-    componentDidMount(){
+    componentDidMount() {
         $(window).on("scroll", function () {
             if ($(window).scrollTop() > 50) {
                 $(".header").addClass("activeHeader");
@@ -11,31 +12,49 @@ class Header extends Component {
                 $(".header").removeClass("activeHeader");
             }
         });
+        $(window).on("scroll", function () {
+            if ($(window).scrollTop() > 50) {
+                $(".logoContainer").addClass("logoContainerBlock");
+            } else {
+                $(".logoContainer").removeClass("logoContainerBlock");
+            }
+        });
+        $(window).on("scroll", function () {
+            if ($(window).scrollTop() > 50) {
+                $(".jusEnd").addClass("jusBetween");
+            } else {
+                $(".jusEnd").removeClass("jusBetween");
+            }
+        });
+
     }
-  render() {
-    return (
-    <div>
-      <nav className="navbar align-items-center navbar-expand-lg p-0 fixed-top header py-3">
-            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ListGroup className="navbar-nav flex-row">
-                    <ListGroupItem className="nav-item  px-3">
-                        <Button className="nav-link px-2 py-1 text-uppercase position-relative activeMenu menu ">Home</Button>
-                    </ListGroupItem>
-                    <ListGroupItem className="nav-item  px-3">
-                        <Button className="nav-link px-2 py-1 text-uppercase position-relative greyColor menu ">About</Button>
-                    </ListGroupItem>
-                    <ListGroupItem className="nav-item  px-3">
-                        <Button className="nav-link px- py-1 text-uppercase position-relative greyColor menu ">Project</Button>
-                    </ListGroupItem>
-                    <ListGroupItem className="nav-item  px-3">
-                        <Button className="nav-link px-2 py-1 text-uppercase position-relative greyColor menu">Contact</Button>
-                    </ListGroupItem>
-                </ListGroup>
+    render() {
+        return (
+            <div>
+                <nav className="navbar align-items-center navbar-expand-lg p-0 fixed-top header py-3">
+                    <div className="collapse navbar-collapse jusEnd" id="navbarSupportedContent">
+                        <div class="logoContainer px-4">
+                            <img src={jrlogo} class="logo" />
+                        </div>
+                        <ListGroup className="navbar-nav flex-row">
+                            <ListGroupItem className="nav-item  px-3">
+                                <Button className="nav-link px-2 py-1 text-uppercase position-relative activeMenu menu ">Home</Button>
+                            </ListGroupItem>
+                            <ListGroupItem className="nav-item  px-3">
+                                <Button className="nav-link px-2 py-1 text-uppercase position-relative greyColor menu ">About</Button>
+                            </ListGroupItem>
+                            <ListGroupItem className="nav-item  px-3">
+                                <Button className="nav-link px- py-1 text-uppercase position-relative greyColor menu ">Project</Button>
+                            </ListGroupItem>
+                            <ListGroupItem className="nav-item  px-3">
+                                <Button className="nav-link px-2 py-1 text-uppercase position-relative greyColor menu">Contact</Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </div>
+                </nav>
             </div>
-        </nav>
-    </div>
-    );
-  }
+        );
+    }
 }
 
 
