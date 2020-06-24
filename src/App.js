45cc4750d68c109382479566848ebcd506c4df09
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-24 18:26:32 +0530

    Custom Cursor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './HomePage/HomePage';
+import CustomCursor from './CustomCursor/CustomCursor';
 
 class App extends Component {
   render() {
     return (
     <div>
+      <CustomCursor />
       <HomePage />
     </div>
     );
