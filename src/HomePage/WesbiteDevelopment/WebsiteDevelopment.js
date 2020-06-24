942eddd5b1954b96ddf788c1772b487cf9c07888
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-24 21:56:55 +0530

    3d view

--- src/HomePage/WesbiteDevelopment/WebsiteDevelopment.js
+++ src/HomePage/WesbiteDevelopment/WebsiteDevelopment.js
@@ -3,6 +3,33 @@ import websiteicon from '../../images/website-icon.svg';
 import laptop from '../../images/laptop.png';
 
 class WebsiteDevelopment extends Component {
+    componentDidMount() {
+        var $wrap = $('#wrapper'),
+            lFollowX = 0,
+            lFollowY = 0,
+            x = 0,
+            y = 0,
+            friction = 1 / 10; 
+
+        function animate() {
+            x += (lFollowX - x) * friction;
+            y += (lFollowY - y) * friction;
+
+            $wrap.css({
+                'transform': 'translate(-50%, -50%) perspective(600px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
+            });
+            window.requestAnimationFrame(animate);
+        }
+
+        $(window).on('mousemove click', function (e) {
+            var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+            var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+            lFollowX = (12 * lMouseX) / 100;
+            lFollowY = (10 * lMouseY) / 100;
+        });
+
+        animate();
+    }
     render() {
         return (
             <div>
@@ -31,7 +58,9 @@ class WebsiteDevelopment extends Component {
                                     </div>
                                 </div>
                                 <div className="col-lg-6">
-                                    <img src={laptop} className="w-100 laptopMargin" alt="Website Development" />
+                                    <div id="wrapper">
+                                        <img src={laptop} className="w-100 laptopMargin" alt="Website Development" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
