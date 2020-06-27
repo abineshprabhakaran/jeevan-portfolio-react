d950e0d314bbae5da8431e4d1bdd6b3455251e44
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-06-27 17:19:59 +0530

    3d

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -10,44 +10,44 @@ import $ from 'jquery';
 class HomePage extends Component {
   componentDidMount() {
     var $wrap = $('#wrapper'),
-        lFollowX = 0,
-        lFollowY = 0,
-        x = 0,
-        y = 0,
-        friction = 1 / 10; 
+      lFollowX = 0,
+      lFollowY = 0,
+      x = 0,
+      y = 0,
+      friction = 1 / 10; 
 
     function animate() {
-        x += (lFollowX - x) * friction;
-        y += (lFollowY - y) * friction;
+      x += (lFollowX - x) * friction;
+      y += (lFollowY - y) * friction;
 
-        $wrap.css({
-            'transform': 'translate(0,0) perspective(0px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
-        });
-        window.requestAnimationFrame(animate);
+      $wrap.css({
+        'transform': 'translate(-50%, -50%) perspective(1000px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
+      });
+      window.requestAnimationFrame(animate);
     }
 
     $(window).on('mousemove click', function (e) {
-        var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-        var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-        lFollowX = (1 * lMouseX) / 100;
-        lFollowY = (1 * lMouseY) / 100;
+      var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+      var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+      lFollowX = (2 * lMouseX) / 100; 
+      lFollowY = (2 * lMouseY) / 100;
     });
 
     animate();
-}
+  }
   render() {
     return (
-    <div>
-      <div id="wrapper">
-      <Container fluid={true}>
-        <Header />
-        <Banner />
-        <WebsiteDevelopment />
-        <MobileDesign />
-        <GraphicDesign />
+      <div>
+        <Container fluid={true}>
+          <Header />
+          <div id="wrapper">
+            <Banner />
+            <WebsiteDevelopment />
+            <MobileDesign />
+            <GraphicDesign />
+          </div>
         </Container>
-        </div>
-    </div>
+      </div>
     );
   }
 }
