c1cdef9c0a094052994d088de78224a060af709e
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2020-07-12 14:31:57 +0530

    Updates

---
+++ precache-manifest.f80dbdf2669108aecf91b1b628a61840.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7b884838f2f035847374a225806e97",
+    "url": "/jeevan-portfolio-react/public/index.html"
+  },
+  {
+    "revision": "3a477e1e7b7cdc87302b",
+    "url": "/jeevan-portfolio-react/public/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "0e942a4ab44ab7de078f",
+    "url": "/jeevan-portfolio-react/public/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a477e1e7b7cdc87302b",
+    "url": "/jeevan-portfolio-react/public/static/js/2.0c41f6e5.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/jeevan-portfolio-react/public/static/js/2.0c41f6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e942a4ab44ab7de078f",
+    "url": "/jeevan-portfolio-react/public/static/js/main.82cdf1f1.chunk.js"
+  },
+  {
+    "revision": "03fb182d56ea484856b7",
+    "url": "/jeevan-portfolio-react/public/static/js/runtime-main.baf076f9.js"
+  },
+  {
+    "revision": "d2761366800b8ae094f4c0f0b759ed57",
+    "url": "/jeevan-portfolio-react/public/static/media/dribbble.d2761366.svg"
+  },
+  {
+    "revision": "c34fe99b249e8d6cc5ad8dd7ccfd0383",
+    "url": "/jeevan-portfolio-react/public/static/media/favicon.c34fe99b.png"
+  },
+  {
+    "revision": "b561fa28cc4c0c1cfe2259572a10ba69",
+    "url": "/jeevan-portfolio-react/public/static/media/footerarrow.b561fa28.svg"
+  },
+  {
+    "revision": "75fe5873b0c93919899e0a11223cae40",
+    "url": "/jeevan-portfolio-react/public/static/media/graphic-black.75fe5873.svg"
+  },
+  {
+    "revision": "fe50c190a1fc5bc78b7ffa28e0c36c03",
+    "url": "/jeevan-portfolio-react/public/static/media/graphic-icon.fe50c190.svg"
+  },
+  {
+    "revision": "9925320441e259941e57d8ba12b5310f",
+    "url": "/jeevan-portfolio-react/public/static/media/graphic.99253204.png"
+  },
+  {
+    "revision": "601792b35ec006d68486141b737fbc46",
+    "url": "/jeevan-portfolio-react/public/static/media/hello-there.601792b3.svg"
+  },
+  {
+    "revision": "3b6069f43d81db505765f215d238098d",
+    "url": "/jeevan-portfolio-react/public/static/media/instagram.3b6069f4.svg"
+  },
+  {
+    "revision": "2e0220f775a0b2204d792eb845668235",
+    "url": "/jeevan-portfolio-react/public/static/media/laptop.2e0220f7.png"
+  },
+  {
+    "revision": "d638d0132e83b99395115a1b3571ab53",
+    "url": "/jeevan-portfolio-react/public/static/media/linkedin.d638d013.svg"
+  },
+  {
+    "revision": "ba7e2a4f98e70f4960d4497dc3c75ad9",
+    "url": "/jeevan-portfolio-react/public/static/media/logo-black.ba7e2a4f.svg"
+  },
+  {
+    "revision": "46952e693f8d607f1ec23417e298c32a",
+    "url": "/jeevan-portfolio-react/public/static/media/logo-icon.46952e69.svg"
+  },
+  {
+    "revision": "60e5b9cb6f0298683fe62d0c5e398832",
+    "url": "/jeevan-portfolio-react/public/static/media/logodesign.60e5b9cb.png"
+  },
+  {
+    "revision": "e765fd2192c05db2d4f2120858b17eb2",
+    "url": "/jeevan-portfolio-react/public/static/media/menu.e765fd21.svg"
+  },
+  {
+    "revision": "13d82e42a0dfc83f0409e3d3df94acce",
+    "url": "/jeevan-portfolio-react/public/static/media/mobile-black.13d82e42.svg"
+  },
+  {
+    "revision": "008dc03d64ce449a46c99f73f95c45f0",
+    "url": "/jeevan-portfolio-react/public/static/media/mobile-icon.008dc03d.svg"
+  },
+  {
+    "revision": "d2be2f931ae0ae219f12df8cb0dba12f",
+    "url": "/jeevan-portfolio-react/public/static/media/mobile.d2be2f93.png"
+  },
+  {
+    "revision": "6fd919d7c7d7aed41796c717bf15ffb4",
+    "url": "/jeevan-portfolio-react/public/static/media/motion-black.6fd919d7.svg"
+  },
+  {
+    "revision": "88c4f457b2cef1763a35dc0ca931d988",
+    "url": "/jeevan-portfolio-react/public/static/media/web-black.88c4f457.svg"
+  },
+  {
+    "revision": "d6ea0ec179fde39170b2882b7699374a",
+    "url": "/jeevan-portfolio-react/public/static/media/website-icon.d6ea0ec1.svg"
+  },
+  {
+    "revision": "9ed8e1b0fb51b7be7e01af28857634ad",
+    "url": "/jeevan-portfolio-react/public/static/media/whatsapp.9ed8e1b0.svg"
+  }
+]);
